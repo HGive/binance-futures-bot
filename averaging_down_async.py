ac033de1d66cd3b1fab549421b9d4315d8c2a845
algorijun <123007169+HGive@users.noreply.github.com>
2024-12-13 23:54:31 +0900

    rsi 기반 로직 완성. 실제 서버 테스트 시작!

--- averaging_down_async.py
+++ averaging_down_async.py
@@ -47,10 +47,11 @@ async def main():
     leverage = 5
     df = None
 
+    buy_count = 0
     entry_price = 0
     position_amount = 0
     pending_tp_order_id = None
-    buy_count = 0
+    pending_sl_order_id = None
 
     # 거래소 초기화
     await exchange.load_markets()
@@ -79,17 +80,32 @@ async def main():
             positions = await exchange.fetch_positions(symbols=[symbol])
 
             # take profit 체결체크
-            if pending_tp_order_id != None:
+            if pending_tp_order_id :
                 pending_tp_order = await exchange.fetch_order(
                     pending_tp_order_id, symbol
                 )
 
-                if pending_tp_order["status"] == "closed":
+                if pending_tp_order["status"] == "closed" :
                     await exchange.cancel_all_orders(symbol=symbol)
                     buy_count = 0
                     entry_price = 0
-                    pending_tp_order_id = None
+                    pending_tp_order_id, pending_sl_order_id = None, None
                     logging.info("---------------   take profit !! ---------------")
+                    continue
+
+            # stop loss 체결체크
+            if pending_sl_order_id :
+                pending_sl_order = await exchange.fetch_order(
+                    pending_sl_order_id, symbol
+                )
+
+                if pending_sl_order["status"] == "closed":
+                    await exchange.cancel_all_orders(symbol=symbol)
+                    buy_count = 0
+                    entry_price = 0
+                    pending_tp_order_id, pending_sl_order_id = None, None
+                    logging.info("---------------   stop loss !! ---------------")
+                    continue
 
             # 포지션
             if positions:
@@ -101,21 +117,32 @@ async def main():
                     await exchange.cancel_all_orders(symbol=symbol)
 
                     tp_price = 1.03 * entry_price
+                    sl_price = 0.97 * current_price
                     tp_order = await exchange.create_order(
                         symbol,
-                        "TAKE_PROFIT",
+                        "TAKE_PROFIT_MARKET",
                         "sell",
                         position_amount,
-                        tp_price,
+                        None,
                         params={"stopPrice": tp_price},
                     )
+                    # sl order
+                    sl_order = await exchange.create_order(
+                        symbol,
+                        "STOP_LOSS_MARKET",
+                        "sell",
+                        position_amount,
+                        None,
+                        params={"stopPrice": sl_price},
+                    )
                     pending_tp_order_id = tp_order["id"]
+                    pending_sl_order_id = sl_order["id"]
 
             # buy 로직
-            if buy_count == 0 and not entry_price and rsi < 29:
+            if buy_count == 0 and not entry_price and rsi < 26 :
                 adjusted_amount = avbl * 0.2 * leverage / current_price
                 tp_price = 1.03 * current_price
-
+                
                 await exchange.cancel_all_orders(symbol=symbol)
 
                 # buy order
@@ -125,17 +152,17 @@ async def main():
                 # tp order
                 tp_order = await exchange.create_order(
                     symbol,
-                    "TAKE_PROFIT",
+                    "TAKE_PROFIT_MARKET",
                     "sell",
                     adjusted_amount,
-                    tp_price,
+                    None,
                     params={"stopPrice": tp_price},
                 )
 
                 pending_tp_order_id = tp_order["id"]
                 buy_count = 1
 
-            elif buy_count == 1 and entry_price and rsi < 19:
+            elif buy_count == 1 and entry_price and ( rsi < 16 or entry_price * 0.93 >= current_price ) :
                 await exchange.cancel_all_orders(symbol=symbol)
 
                 adjusted_amount = avbl * 1 * leverage / current_price
