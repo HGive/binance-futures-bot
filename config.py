e173d93543830856ce90961662096101542f05ad
HGive <skyjoon34@naver.com>
2025-07-20 15:01:28 +0900

    fix .env error

--- config.py
+++ config.py
@@ -11,6 +11,8 @@ logging.Formatter.converter = timetz
 # 로그 디렉토리 생성
 os.makedirs("logs", exist_ok=True)
 
+load_dotenv()
+
 # 환경변수에서 로그 파일명 가져오기 (기본값: hour_3p_strategy.log)
 log_filename = os.environ.get("LOG_FILENAME", "hour_3p_strategy.log")
 log_path = os.path.join("logs", log_filename)
@@ -23,6 +25,5 @@ logging.basicConfig(
     encoding="utf-8"  # 한글 로그 깨짐 방지
 )
 
-load_dotenv()
 # exchange = None
 exchange = ccxt.binance({"apiKey": os.environ["BINANCE_API_KEY"], "secret": os.environ["BINANCE_API_SECRET"], "enableRateLimit": True, "options": {"defaultType": "future", "adjustForTimeDifference": True}})
